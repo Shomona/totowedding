82bfb53627596cf63032b8d85c5b5ad906ebc9ab
Shomona <shomona.mukherjee@gmail.com>
2019-06-16 14:41:58 -0700

    Updates

---
+++ precache-manifest.ec69038afe66eea85ea9e6e5e5ab2893.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1589cf9384063c86626bd455bf0aa4",
+    "url": "/totowedding/index.html"
+  },
+  {
+    "revision": "fb3f74d9782cee427896",
+    "url": "/totowedding/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/totowedding/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "fb3f74d9782cee427896",
+    "url": "/totowedding/static/js/main.f4f8d529.chunk.js"
+  },
+  {
+    "revision": "d9d4a8a54c8968161eb1",
+    "url": "/totowedding/static/js/runtime~main.c5fefda0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/totowedding/static/media/logo.5d5d9eef.svg"
+  }
+]);
